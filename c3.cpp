1076fec495c8c8a9610f6eb8f36a3f2adc64d53c
Abhinav Verma <121615395+abhinavVerma199@users.noreply.github.com>
2025-01-29 22:04:42 +0530

    stack1(c3)

--- c3.cpp
+++ c3.cpp
@@ -1,37 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    bool solve(TreeNode* root, int targetSum, int sum ){
-        //base case
-        if(root == NULL) {
-            return false;
-        }
-        sum += root->val;
-        if(root->left == NULL && root->right==NULL) {
-            //i m currently standing on a leaf node
-            //verify
-            if(sum == targetSum) 
-                return true;
-            else
-                return false;
-        }
-        bool leftAns = solve(root->left, targetSum, sum);
-        bool rightAns = solve(root->right,targetSum, sum);
-        return leftAns||rightAns;
-    }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum = 0;
-        bool ans = solve(root, targetSum, sum);
-        return ans;  
-    }
-};
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+  string str = "hellojee";
+  stack<char> st;
+
+  for(int i=0; i<str.length(); i++) {
+    char ch = str[i];
+    st.push(ch);
+  }
+
+  cout << endl;
+  while( !st.empty() ) {
+    cout << st.top()  ;
+    st.pop();
+  }cout << endl;
+
+  return 0;
+}
